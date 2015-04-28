c663bda5675a5816eeff22a80efc05f0310e7e56
Darryl Anaud <darrylanaud@Darryls-MacBook-Pro.local>
2015-04-28 15:58:37 +0800

    added surveys table

---
+++ database/migrations/2015_04_28_074307_create_surveys_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('surveys', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('welcome_message');
+            $table->string('end_message');
+            $table->string('url');
+            $table->text('instructions');
+            $table->integer('question_set_id')->unsigned();
+            $table->integer('faculty_id')->unsigned();
+            $table->integer('schedule_id')->unsigned();
+            $table->boolean('active');
+            $table->dateTime('expires');
+            $table->dateTime('start_date');
+            $table->dateTime('deactivated_date');
+            $table->integer('deactivated_by_id')->unsigned();
+            $table->integer('created_by_id')->unsigned();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('surveys');
+	}
+
+}
