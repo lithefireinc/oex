8d94232cf61a0f1a28c02c90d9716dad9f76fd47
richardmalkuth <richardmalkuth@gmail.com>
2015-05-16 17:07:18 +0800

    added grid table to fetch data (incomplete)

--- resources/views/surveys/index.blade.php
+++ resources/views/surveys/index.blade.php
@@ -1,6 +1,29 @@
 @extends('app')
 @section('content')
 
-Surveys
+    <h1 class="page-heading">Surveys</h1>
+
+    <table class="table table-striped table-bordered">
+
+        <thead>
+            <th>Title:</th>
+            <th>Question Set:</th>
+            <th>Faculty:</th>
+            <th>Duration:</th>
+        </thead>
+
+        <tbody>
+            @foreach($surveys as $survey)
+                <tr>
+                    <td>{{ $survey->title }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
+
+    @unless(count($surveys))
+        <p class="text-center">You haven't add any surveys yet!</p>
+    @endunless
 
 @stop
