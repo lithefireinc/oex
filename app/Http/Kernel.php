bb47fc4ffb35178554d4a3b78b9b6d04661ecdf8
richardmalkuth <richardmalkuth@gmail.com>
2015-05-17 20:34:02 +0800

    add roles in routemiddleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel {
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		'App\Http\Middleware\VerifyCsrfToken',
-        'roles' 		=> 'App\Http\Middleware\CheckRole',
 	];
 
 	/**
@@ -28,6 +27,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
+        'roles' => 'App\Http\Middleware\CheckRole',
 	];
 
 }
