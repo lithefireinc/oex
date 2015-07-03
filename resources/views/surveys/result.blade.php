1bee36c37667491c9203b50f611ead95b211cb58
richardmalkuth <richardmalkuth@gmail.com>
2015-07-03 12:23:10 +0800

    modify results table and made quesions row fixed

--- resources/views/surveys/result.blade.php
+++ resources/views/surveys/result.blade.php
@@ -2,32 +2,35 @@
 @section('content')
 
     <h1 class="page-heading">Evaluation of {!! $survey->faculty->ADVISER !!} Results</h1>
-    <div class="table_results_wrapper">
-    <table class="table table-striped table-bordered table_results">
-        <thead>
-            <th>Questions</th>
-            <?php $count = 1; ?>
-            @foreach( $results as $result )
-                <th>Respondent {!! $count !!}</th>
-            <?php $count++; ?>
-        @endforeach
-        </thead>
-        <tbody>
-            @foreach( $question_categories as $question_category ) @foreach( $question_category->questions()->orderBy('order')->get() as $question )
-                <tr>
-                    <td>
-                        {!! $question->question !!}
-                    </td>
-                    @foreach( $results as $result )
-                        <td>
-                            <?php $column_name = $field_name.$question->id;?>
-                            {!! $result->$column_name !!}
-                        </td>
-                    @endforeach
-                </tr>
-            @endforeach @endforeach
-        </tbody>
-    </table>
+
+    <div class="outer">
+        <div class="inner">
+            <table class="table table-striped table-bordered table_survey_results">
+                <thead>
+                <th>Questions</th>
+                <?php $count = 1; ?>
+                @foreach( $results as $result )
+                    <td>Respondent {!! $count !!}</td>
+                    <?php $count++; ?>
+                @endforeach
+                </thead>
+                <tbody>
+                @foreach( $question_categories as $question_category ) @foreach( $question_category->questions()->orderBy('order')->get() as $question )
+                    <tr>
+                        <th>
+                            {!! $question->question !!}
+                        </th>
+                        @foreach( $results as $result )
+                            <td>
+                                <?php $column_name = $field_name.$question->id;?>
+                                {!! $result->$column_name !!}
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
 @stop
