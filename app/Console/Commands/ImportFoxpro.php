bab69374679ab7e69181c091bdc70850a6328646
Darryl Anaud <darrylanaud@gmail.com>
2015-07-09 13:16:45 +0800

    Corrected FILESTTY

--- app/Console/Commands/ImportFoxpro.php
+++ app/Console/Commands/ImportFoxpro.php
@@ -29,7 +29,7 @@ class ImportFoxpro extends Command
         "FILEROOM",
         "FILESECT",
         "FILESTLE",
-        "FILESSTTY",
+        "FILESTTY",
         "FILETIME",
         "SCHEFILE",
         "SUBJFILE",
